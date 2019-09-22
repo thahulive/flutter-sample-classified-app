36cdefd6d024f406cdecaddd3713b513686f89b7
Aboo Thahir <thahulive@gmail.com>
2019-09-22 23:59:02 +0530

    login, home  pages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ente_dweep_app/main.dart';
+import 'package:classifieds_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
